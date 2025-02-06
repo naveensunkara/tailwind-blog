6f7beacfd34373600387b8f9557741d030806b1c
Naveen Kumar Sunkara <naveen241193@gmail.com>
2025-02-06 17:51:09 +0000

    new attempt

--- auth.ts
+++ auth.ts
@@ -1,11 +1,11 @@
-import NextAuth from "next-auth";
-import GitHub from "next-auth/providers/github";
+import NextAuth from 'next-auth'
+import GitHub from 'next-auth/providers/github'
 
 export const {
-    handlers: { GET, POST },
+  handlers: { GET, POST },
   auth,
   signIn,
   signOut,
 } = NextAuth({
   providers: [GitHub],
-});
+})
